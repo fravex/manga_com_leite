9cf5a7d104c0abab2cf8375f566967233e64c5b0
fravex <97367854+fravex@users.noreply.github.com>
2022-03-27 11:20:40 -0300

    Update settings.py

--- interacao_medicamento/settings.py
+++ interacao_medicamento/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-76vti7t^7*cl$+a(xr%4@sjx^%ti+u#2dppfj@%p743uei^$s*'
+SECRET_KEY = 'PUT_YOUR_DJANGO_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
